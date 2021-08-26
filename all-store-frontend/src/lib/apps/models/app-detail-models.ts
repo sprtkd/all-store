7a6b92e25601ced315417d9cd15f4feb086c35f2
Suprotik Dey <supdey1@gmail.com>
2021-08-27 02:20:26 +0530

    sync

--- all-store-frontend/src/lib/apps/models/app-detail-models.ts
+++ all-store-frontend/src/lib/apps/models/app-detail-models.ts
@@ -1,10 +1,48 @@
+import { Review } from './../../review/models/review-models';
 export interface DetailedAppModel {
     id: string;
     basic: {
         name: string;
-        icon: string;
-        stars: number;
         downloads: number;
         size: number;
+        version: string;
+    },
+    details: {
+        miniAbout: string;
+        about: string;
+        alert: string[];
+        lastUpdateAddition: string[];
+        lastUpdatedOn: Date;
+        releasedOn: Date;
     }
+    vendor: {
+        vendorId: string;
+        vendorName: string;
+        vendorUrl: string;
+    },
+    media: {
+        icon: string;
+        backDrop: string;
+        carousel: string[];
+    },
+    flags: {
+        isMobileFriendly: boolean;
+        isSafeForKids: boolean;
+        isBeta: boolean;
+        isOpenSource: boolean;
+        isAdSupported: boolean;
+        hasInAppPurchase: boolean;
+        isTrial: boolean;
+        requiresHardwareAccess: boolean;
+    },
+    devicePermissions: string[];
+    supportedDevices: string[];
+    tags: string[];
+    review: {
+        stars: number;
+        reviewCount: number;
+        reviewId: Review[];
+        reviewTags: string[];
+    },
+    appCategories: string[];
 }
